e6de316a6141c9fe10f2e9af00a79495cbd7d130
Paweł Bylica <chfast@gmail.com>
2017-02-28 08:34:00 +0100

    WIP: limit tests to BlockChain

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -40,7 +40,7 @@ if [[ "$TESTS" == "On" ]]; then
 
     # Run the tests for the Interpreter
     cd cpp-ethereum/build
-    $BUILD_ROOT/test/testeth
+    $BUILD_ROOT/test/testeth -t "*BlockChain*"
 
     # Run the tests for the JIT (but only for Ubuntu, not macOS)
     # The whole automation process is too slow for macOS, and we don't have
