500a194a76950840f3fb8539dd46e8401fa8b1bf
Jordan Earls <earlz@earlz.net>
2017-08-02 21:30:56 -0400

    Fix weird sha3.h, just hardcode for now as I can't resolve undefined symbol errors

--- libethash/internal.c
+++ libethash/internal.c
@@ -38,7 +38,40 @@
 #include "sha3_cryptopp.h"
 
 #else
-#include "sha3.h"
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "compiler.h"
+#include <stdint.h>
+#include <stdlib.h>
+
+struct ethash_h256;
+
+#define decsha3(bits) \
+	int sha3_##bits(uint8_t*, size_t, uint8_t const*, size_t);
+
+decsha3(256)
+decsha3(512)
+
+void SHA3_256(struct ethash_h256 const* ret, uint8_t const* data, size_t const size)
+{
+	sha3_256((uint8_t*)ret, 32, data, size);
+}
+
+void SHA3_512(uint8_t* ret, uint8_t const* data, size_t const size)
+{
+	sha3_512(ret, 64, data, size);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+
 #endif // WITH_CRYPTOPP
 
 uint64_t ethash_get_datasize(uint64_t const block_number)
