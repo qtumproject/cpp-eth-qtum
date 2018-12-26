7d8a3e5eab11abfe6d245e85669246ded757c583
Time Markov <time.markov@qtum.com>
2018-12-26 09:58:07 +0100

    Remove cryptopp version check for qtum

--- libdevcrypto/CryptoPP.cpp
+++ libdevcrypto/CryptoPP.cpp
@@ -27,7 +27,9 @@
 #include <libdevcore/Assertions.h>
 #include <libdevcore/SHA3.h>
 
+#ifndef QTUM_BUILD
 static_assert(CRYPTOPP_VERSION == 565, "Wrong Crypto++ version");
+#endif
 
 using namespace dev;
 using namespace dev::crypto;
