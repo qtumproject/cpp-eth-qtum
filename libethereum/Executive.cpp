SIProjects/cpp-eth-sicash

createdContracts and destructedContracts

libethereum/Executive.cpp
@@ -488,6 +488,7 @@ bool Executive::go(OnOpFunc const& _onOp)
                     if (m_res)
                         m_res->codeDeposit = CodeDeposit::Success;
                     m_gas -= out.size() * m_ext->evmSchedule().createDataGas;
+                    m_s.createdContracts().push_back({m_newAddress, m_ext->code});
                 }
                 else
                 {
@@ -579,8 +580,10 @@ bool Executive::finalize()
 
     // Suicides...
     if (m_ext)
-        for (auto a: m_ext->sub.suicides)
+        for (auto a: m_ext->sub.suicides) {
             m_s.kill(a);
+            m_s.destructedContracts().push_back(a);
+        }
 
     // Logs..
     if (m_ext)

qtumproject/cpp-eth-qtum

libethereum/Executive.cpp
@@ -362,6 +362,7 @@ bool Executive::go(OnOpFunc const& _onOp)
                     if (m_res)
                         m_res->codeDeposit = CodeDeposit::Success;
                     m_gas -= out.size() * m_ext->evmSchedule().createDataGas;
+                    m_s.createdContracts().push_back({m_newAddress, out.toVector()});
                 }
                 else
                 {
@@ -454,8 +455,10 @@ bool Executive::finalize()
 
     // Selfdestructs...
     if (m_ext)
-        for (auto a: m_ext->sub.selfdestructs)
+        for (auto a: m_ext->sub.selfdestructs) {
             m_s.kill(a);
+            m_s.destructedContracts().push_back(a);
+        }
 
     // Logs...
     if (m_ext)